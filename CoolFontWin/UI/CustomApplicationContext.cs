12c3ccfe0fa3b7ffd52ce273d5d44bf730e57bc4
Mitchell McMillan <mitchellmcm27@gmail.com>
2016-10-26 11:27:21 -0500

    auto updating done

--- CoolFontWin/UI/CustomApplicationContext.cs
+++ CoolFontWin/UI/CustomApplicationContext.cs
@@ -183,8 +183,6 @@ namespace CoolFont.AppWinForms
 
         #region updating
 
-        long sizeOfUpdate = 0;
-
         public void CheckForUpdates()
         {
             if (ApplicationDeployment.IsNetworkDeployed)
@@ -216,25 +214,8 @@ namespace CoolFont.AppWinForms
                // MessageBox.Show("The update was cancelled.");
             }
 
-            // Ask the user if they would like to update the application now.
             if (e.UpdateAvailable)
             {
-                sizeOfUpdate = e.UpdateSizeBytes;
-                /*
-                if (!e.IsUpdateRequired)
-                {
-                    DialogResult dr = MessageBox.Show("An update is available. Would you like to update the application now?", "Update Available", MessageBoxButtons.OKCancel);
-                    if (DialogResult.OK == dr)
-                    {
-                        BeginUpdate();
-                    }
-                }
-                else
-                {
-                    MessageBox.Show("A mandatory update is available for your application. We will install the update now, after which we will save all of your in-progress data and restart your application.");
-                    BeginUpdate();
-                }
-                */
                 BeginUpdate();
             }
         }
@@ -266,84 +247,7 @@ namespace CoolFont.AppWinForms
                 return;
             }
 
-            
-            // update on next launch
-            /*
-            DialogResult dr = MessageBox.Show("The application has been updated. Restart? (If you do not restart now, the new version will not take effect until after you quit and launch the application again.)", "Restart Application", MessageBoxButtons.OKCancel);
-            if (DialogResult.OK == dr)
-            {
-                Application.Restart();
-            }
-            */                    
-        }
-
-        // not used...
-        private void InstallUpdateSyncWithInfo()
-        {
-            UpdateCheckInfo info = null;
-
-            if (ApplicationDeployment.IsNetworkDeployed)
-            {
-                ApplicationDeployment ad = ApplicationDeployment.CurrentDeployment;
-
-                try
-                {
-                    info = ad.CheckForDetailedUpdate();
-                }
-                catch (DeploymentDownloadException dde)
-                {
-                    MessageBox.Show("The new version of the application cannot be downloaded at this time. \n\nPlease check your network connection, or try again later. Error: " + dde.Message);
-                    return;
-                }
-                catch (InvalidDeploymentException ide)
-                {
-                    MessageBox.Show("Cannot check for a new version of the application. The ClickOnce deployment is corrupt. Please redeploy the application and try again. Error: " + ide.Message);
-                    return;
-                }
-                catch (InvalidOperationException ioe)
-                {
-                    MessageBox.Show("This application cannot be updated. It is likely not a ClickOnce application. Error: " + ioe.Message);
-                    return;
-                }
-
-                if (info.UpdateAvailable)
-                {
-                    Boolean doUpdate = true;
-
-                    if (!info.IsUpdateRequired)
-                    {
-                        DialogResult dr = MessageBox.Show("An update is available. Would you like to update the application now?", "Update Available", MessageBoxButtons.OKCancel);
-                        if (!(DialogResult.OK == dr))
-                        {
-                            doUpdate = false;
-                        }
-                    }
-                    else
-                    {
-                        // Display a message that the app MUST reboot. Display the minimum required version.
-                        MessageBox.Show("This application has detected a mandatory update from your current " +
-                            "version to version " + info.MinimumRequiredVersion.ToString() +
-                            ". The application will now install the update and restart.",
-                            "Update Available", MessageBoxButtons.OK,
-                            MessageBoxIcon.Information);
-                    }
-
-                    if (doUpdate)
-                    {
-                        try
-                        {
-                            ad.Update();
-                            MessageBox.Show("The application has been upgraded, and will now restart.");
-                            Application.Restart();
-                        }
-                        catch (DeploymentDownloadException dde)
-                        {
-                            MessageBox.Show("Cannot install the latest version of the application. \n\nPlease check your network connection, or try again later. Error: " + dde);
-                            return;
-                        }
-                    }
-                }
-            }
+            // update will apply on next startup                 
         }
         #endregion
     
