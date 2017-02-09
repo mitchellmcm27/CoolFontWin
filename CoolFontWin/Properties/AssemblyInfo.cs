badb9cbf324825c0c2cd3ec87065efec950dddb7
Mitchell McMillan <mitchellmcm27@gmail.com>
2017-02-09 15:35:37 -0600

    assembly info version 1.3.3

--- CoolFontWin/Properties/AssemblyInfo.cs
+++ CoolFontWin/Properties/AssemblyInfo.cs
@@ -35,7 +35,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.3.2.0")] // only change for non-backwards compatible builds
+[assembly: AssemblyVersion("1.3.3.0")] // only change for non-backwards compatible builds
 [assembly: AssemblyInformationalVersion("1.3")] // consumer-facing description of project version, could be anything
 
 // Logging
