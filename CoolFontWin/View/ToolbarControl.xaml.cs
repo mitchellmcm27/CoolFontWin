1fe9326ebe4064be3c3689cef867b6788ff3bebd
Mitchell McMillan <mitchellmcm27@gmail.com>
2017-06-20 21:06:40 -0500

    ui design changes

--- CoolFontWin/View/ToolbarControl.xaml.cs
+++ CoolFontWin/View/ToolbarControl.xaml.cs
@@ -25,5 +25,10 @@ namespace CFW.View
             var myWindow = Window.GetWindow(this);
             myWindow.WindowState = WindowState.Minimized;
         }
+
+        protected void Window_MouseLeftButtonDown(object sender, RoutedEventArgs e)
+        {
+            Application.Current.MainWindow.DragMove();
+        }
     }
 }
