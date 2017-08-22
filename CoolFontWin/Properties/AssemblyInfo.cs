c6386fae460217bbbc72579395070ba8ad723df9
Mitchell McMillan <mitchellmcm27@gmail.com>
2017-08-21 21:54:03 -0400

    release v 1.4

--- CoolFontWin/Properties/AssemblyInfo.cs
+++ CoolFontWin/Properties/AssemblyInfo.cs
@@ -35,8 +35,8 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.3.4.0")] // only change for non-backwards compatible builds
-[assembly: AssemblyInformationalVersion("1.3")] // consumer-facing description of project version, could be anything
+[assembly: AssemblyVersion("1.4.0")] // only change for non-backwards compatible builds
+[assembly: AssemblyInformationalVersion("1.4")] // consumer-facing description of project version, could be anything
 
 // Logging
 [assembly: log4net.Config.XmlConfigurator(Watch = true)]
