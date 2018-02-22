a2c3dbe61c25a0643d0e62c32a524f1ca4dc870a
Mitchell McMillan <mitchellmcm27@gmail.com>
2018-02-21 20:52:00 -0500

    version bump

--- CoolFontWin/Properties/AssemblyInfo.cs
+++ CoolFontWin/Properties/AssemblyInfo.cs
@@ -34,8 +34,8 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.5.5")] // only change for non-backwards compatible builds
-[assembly: AssemblyInformationalVersion("1.5.5")] // consumer-facing description of project version, could be anything
+[assembly: AssemblyVersion("1.5.6")] // only change for non-backwards compatible builds
+[assembly: AssemblyInformationalVersion("1.5.6")] // consumer-facing description of project version, could be anything
 
 // Logging
 [assembly: log4net.Config.XmlConfigurator(Watch = true)]
